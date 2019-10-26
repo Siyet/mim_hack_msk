f6d60a9a6cb9f2b800adedc1bb17c72504fac30f
Siyet <a.s.tseluyko@gmail.com>
2019-10-26 06:26:39 +0300

    1

--- backend/inserting2db.js
+++ backend/inserting2db.js
@@ -2,9 +2,9 @@ const fs = require("fs"),
   nano = require("nano")("http://admin:vbyjvtn@tseluyko.ru:5984"),
   db = nano.db.use("mim");
 
-let decls = JSON.parse(fs.readFileSync("tmp/declarations.json"));
+// let decls = JSON.parse(fs.readFileSync("tmp/declarations.json"));
 
-console.log(decls.length);
+// console.log(decls.length);
 
 const StreamArray = require("stream-json/streamers/StreamArray");
 const path = require("path");
