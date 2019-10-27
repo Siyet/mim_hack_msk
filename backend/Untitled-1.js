30ba5fd0b2c7f8f94f95667a5816407a4a029a9d
Siyet <a.s.tseluyko@gmail.com>
2019-10-27 04:19:53 +0300

    1

--- backend/Untitled-1.js
+++ backend/Untitled-1.js
@@ -1,120 +1,60 @@
-let aaa = {
-  _id: "0916aa1a7ca84fcff338ce3f94000b85",
-  _rev: "1-8e8aef1fa7edf6fd625523b3bd67f00f",
-  main: {
-    person: {
-      id: 3460,
-      name: "Котлярович Людмила Васильевна",
-      family_name: "Котлярович",
-      given_name: "Людмила",
-      patronymic_name: "Васильевна",
-      gender: "F"
-    },
-    office: {
-      id: 1405,
-      name: "Гусевский - городской округ",
-      url: "http://www.admgusev.ru/",
-      type: {
-        id: 20,
-        name: "Муниципальный, без структуры"
-      },
-      region: {
-        id: 46,
-        name: "Калининградская область"
-      }
-    },
-    party: null,
-    year: 2013,
-    document_type: {
-      id: 1,
-      name: "Антикоррупционная декларация"
-    }
-  },
-  incomes: [
-    {
-      size: 669943,
-      relative: null,
-      comment: ""
-    }
-  ],
-  real_estates: [
-    {
-      name: "",
-      square: 66.7,
-      country: "Россия",
-      region: null,
-      comment: "",
-      type: {
-        id: 3,
-        name: "Жилой дом"
-      },
-      own_type: {
-        id: 22,
-        name: "Совместная собственность"
-      },
-      share: 0.5,
-      relative: null
-    },
-    {
-      name: "",
-      square: 300,
-      country: "Россия",
-      region: null,
-      comment: "",
-      type: {
-        id: 1,
-        name: "Земельный участок"
-      },
-      own_type: {
-        id: 20,
-        name: "Индивидуальная"
-      },
-      share: null,
-      relative: {
-        id: 2,
-        name: "Супруг(а)"
-      }
-    },
-    {
-      name: "",
-      square: 66.7,
-      country: "Россия",
-      region: null,
-      comment: "",
-      type: {
-        id: 3,
-        name: "Жилой дом"
-      },
-      own_type: {
-        id: 21,
-        name: "Долевая собственность"
-      },
-      share: 0.5,
-      relative: {
-        id: 2,
-        name: "Супруг(а)"
-      }
-    }
-  ],
-  vehicles: [],
-  savings: [],
-  stocks: [],
-  bonds: [],
-  spendings: [],
-  country: "Russia"
-};
+// by_country
+function (doc) {
+  if (!doc) emit('wrong', 1);
+  if (!doc || !doc.country) return;
+  emit(doc.country, 1);
+}
 
-function test(doc) {
-  if (!doc || !Array.isArray(doc.incomes) || !doc.main || !doc.main.year)
-    return;
+// by_income
+function (doc) {
+  if (!doc || !Array.isArray(doc.incomes) || !doc.main || !doc.main.year) return;
   var income = 0;
   var _income = 0;
-  for (var idx = 0; idx < doc.incomes.length; idx++) {
+  for (var idx=0; idx < doc.incomes.length; idx++){
     if (!income && !doc.incomes[idx].relative) income = doc.incomes[idx].size;
     if (!_income) _income = 1;
     _income += doc.incomes[idx].size;
   }
-  console.log([false, doc.main.year], income);
-  console.log([true, doc.main.year], _income);
+  emit([false, doc.main.year], income);
+  emit([true, doc.main.year], _income);
 }
-test(aaa);
+// by_possessions
+function (doc) {
+  if (!doc || !doc.main || !doc.main.person || !doc.main.person.family_name || !doc.main.year) return;
+  var title = doc.main.person.family_name
+  if (doc.main.person.given_name) title += ' ' + doc.main.person.given_name.substr(0,1).toUpperCase() + '.'
+  if (doc.main.person.patronymic_name) title += ' ' + doc.main.person.patronymic_name.substr(0,1).toUpperCase() + '.'
+  if (Array.isArray(doc.real_estates) && doc.main.year >= (new Date().getFullYear()-1)){
+    var square = 0;
+    for (var idx = 0; idx < doc.real_estates.length; idx++){
+      if (doc.real_estates[idx] && doc.real_estates[idx].square) square += doc.real_estates[idx].share ? doc.real_estates[idx].square * doc.real_estates[idx].share : doc.real_estates[idx].square;
+    }
+    if (square > 1) emit('real_estates', [title,square])
+  } 
+  if (Array.isArray(doc.vehicles) && doc.vehicles.length && doc.main.year >= (new Date().getFullYear()-2)){
+    emit('vehicles', [title, doc.vehicles.length])
+  }
+}
+// by_regions
+function (doc) {
+  if (!doc || !doc.main || !doc.main.year || !doc.main.office|| !doc.main.office.region|| doc.main.year < (new Date().getFullYear()-2)) return;
+  if (doc.main.year < (new Date().getFullYear()-1)){
+    if (Array.isArray(doc.incomes) && doc.incomes.length){
+      var income = 0;
+      for(var idx=0; idx < doc.incomes.length; idx++){
+        if (doc.incomes[idx] && doc.incomes[idx].size) income += doc.incomes[idx].size
+      }
+      emit([doc.main.office.region.name, 'incomes'], income)
+    }
+    if (Array.isArray(doc.real_estates) && doc.real_estates.length){
+      var real_estates = 0;
+      for(var idx=0; idx < doc.real_estates.length; idx++){
+        if (doc.real_estates[idx] && doc.real_estates[idx].square) real_estates += doc.real_estates[idx].share ? doc.real_estates[idx].square *doc.real_estates[idx].share : doc.real_estates[idx].square
+      }
+      emit([doc.main.office.region.name, 'real_estates'], real_estates)
+    }
+  }
+  if (Array.isArray(doc.vehicles) && doc.vehicles.length){
+    emit([doc.main.office.region.name, 'vehicles'], doc.vehicles.length)
+  }
+}
--- backend/migrate.js
+++ backend/migrate.js
@@ -3,7 +3,7 @@ const fs = require("fs"),
   db = nano.db.use("mim");
 
 (async () => {
-  let ds = await db.view("daclarations", "by_country", {
+  let ds = await db.list({
     include_docs: false,
     reduce: false
   });
@@ -12,11 +12,19 @@ const fs = require("fs"),
     let part =
       ds.rows.length > 1000
         ? ds.rows.splice(0, 1000)
-        : ds.rows.splice(0, db.rows.length);
+        : ds.rows.splice(0, ds.rows.length);
+    console.log(ds.rows.length, part.length);
+
     await db.bulk({
       docs: part.map(item => {
-        item._id =
-          item.main && item.main.person ? item.main.person.id : item._id;
+        if (!item.main) item._deleted = true;
+        else {
+          item._id = parseStr(
+            item.main && item.main.person ? item.main.person.id : item._id
+          );
+        }
+
+        return item;
       })
     });
   }
