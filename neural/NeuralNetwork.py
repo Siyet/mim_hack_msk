a8cb437d26bf45c1605215f8e874bfb7289d95f7
Siyet <a.s.tseluyko@gmail.com>
2019-10-27 06:43:25 +0300

    add neural

---
+++ neural/NeuralNetwork.py
@@ -0,0 +1,29 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from sklearn.model_selection import train_test_split
+import numpy
+
+numpy.random.seed(2)
+
+dataset = numpy.loadtxt("DATASET.csv", delimiter=",")
+
+X = dataset[:,0:2]
+Y = dataset[:,2]
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+model = Sequential()
+model.add(Dense(15, input_dim=2, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dropout(.2))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs = 5000, batch_size=20, validation_data=(x_test, y_test))
+
+scores = model.evaluate(X, Y)
+print(model.metrics_names[1], scores[1]*100)
+
+model.save('weights.h5')
